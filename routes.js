12a0f6eda86b1dc6ebf59cdf6a77cf332ccac3db
ameyachavan007 <chavanameya007@gmail.com>
2024-01-22 15:26:21 -0500

    server-v2.0
    -added new routes for profile sharing
    -added routes for creating profile
    -cleaned up unused routes

--- routes.js
+++ routes.js
@@ -1,17 +1,36 @@
 const express = require("express");
+const session = require("express-session");
 const router = express.Router();
 const User = require("./user");
 
+// Configure express-session
+const sessionOptions = {
+  secret: "this is my session key axios@10144", // Replace with your secret key
+  resave: false,
+  saveUninitialized: false,
+  // cookie: { secure: false } // Set to true if using https
+};
+
+router.use(session(sessionOptions));
+
 //Sign-up route
-router.post("/singup", async (req, res) => {
-  let user = new User();
-  user.email = req.body.email;
-  user.password = req.body.password;
+router.post("/signup", async (req, res) => {
+  const { email, username } = req.body;
 
   try {
-    const doc = await user.save();
-    console.log(doc);
-    res.json(doc);
+    let userByUsername = await User.findOne({ username });
+    if (userByUsername) {
+      return res.status(403).json({ error: "Username already exists" });
+    }
+    let userByEmail = await User.findOne({ email });
+    if (userByEmail) {
+      return res.status(403).json({ error: "User already exists" });
+    }
+
+    return res.status(200).json({
+      // user: doc,
+      message: "SignUp successful",
+    });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -19,58 +38,91 @@ router.post("/singup", async (req, res) => {
 
 //Login route
 router.post("/", async (req, res) => {
+  const { email, hashedPassword } = req.body;
+
   try {
-    const user = await User.findOne({ email: req.body.email });
+    const user = await User.findOne({ email: email });
     if (!user) {
+      // Send a JSON response instead of redirecting
       return res.status(404).json({ error: "User not found" });
     }
 
-    const isMatch = req.body.password === user.password;
-    if (!isMatch) {
+    if (hashedPassword !== user.password) {
       return res.status(401).json({ error: "Invalid password" });
     }
-
+    req.session.userId = user._id;
     res.json({ message: "Login successful", user: user });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
 
-// about route
-router.put("/:userName/about", async (req, res) => {
-  try {
-    const { about } = req.body;
-    const updatedUser = await User.findOneAndUpdate(
-      { firstName: req.params.userName },
-      { about: req.body.about },
-      { new: true }
-    );
-    if (!updatedUser) {
-      return res.status(404).json({ error: "User not found" });
-    }
-    res.json({ message: "About data updated successfully", user: updatedUser });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+// career-details route
+router.post("/career-details", async (req, res) => {
+  const {
+    email,
+    password,
+    firstName,
+    lastName,
+    about,
+    experiences,
+    username,
+    projects,
+    github,
+    instagram,
+    twitter,
+    linkedin,
+    tagLine,
+  } = req.body.user;
 
-// experience route
-router.put("/:userName/experience", async (req, res) => {
-  try{
-    const { experience } = req.body;
-    const user = await User.findOne({ firstName: req.params.userName });
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-    // Assuming you want to add the new experience to the existing ones
-    user.experiences.push(experience);
-    // Save the updated user
-    const updatedUser = await user.save();
-    res.json({ user: updatedUser });
-  } catch(err){
-    console.error(err);
+  const parsedExperiences = JSON.stringify(experiences);
+  const parsedProjects = JSON.stringify(projects);
+  try {
+    const updateData = {
+      username: username,
+      email: email,
+      password: password,
+      firstName: firstName,
+      lastName: lastName,
+      about: about,
+      experiences: parsedExperiences,
+      projects: parsedProjects,
+      github: github,
+      instagram: instagram,
+      twitter: twitter,
+      linkedin: linkedin,
+      tagLine: tagLine,
+    };
+    const user = await User.findOneAndUpdate(
+      { username: username },
+      updateData,
+      { new: true, upsert: true, setDefaultsOnInsert: true }
+    );
+    res.status(200).json({ user: user });
+  } catch (error) {
     res.status(500).json({ message: "Server error" });
   }
 });
 
+router.get("/api/check-session", (req, res) => {
+  if (req.session.userId) {
+    // The user is logged in, return a success status
+    res.status(200).json({ message: "Authenticated" });
+  } else {
+    // The user is not logged in, return an error status
+    res.status(401).json({ message: "Not Authenticated" });
+  }
+});
+
+router.get("/:username", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.params.username });
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    res.status(200).json({ user: user });
+  } catch (error) {
+    res.status(500).send("Internal Server Error");
+  }
+});
 module.exports = router;
--- user.js
+++ user.js
@@ -1,10 +1,33 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  email: String,
+  email: {
+    type: String,
+    unique: true, // This ensures that the email is unique in the collection
+    required: true, // This makes the email field required
+    trim: true, // Trims whitespace from the email
+    lowercase: true, // Converts the email to lowercase
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    lowercase: true,
+  },
   password: String,
   about: String,
   experiences: Array,
+  projects: Array,
+  current_company: String,
+  current_position: String,
+  firstName: String,
+  lastName: String,
+  github: String,
+  instagram: String,
+  twitter: String,
+  linkedin: String,
+  tagLine: String,
 });
 
 const User = mongoose.model("User", userSchema);
